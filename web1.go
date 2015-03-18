b343ecc9dc58a3be16130993f2652a4e75ed69f1
Alaikya Kasi <alaikyakasireddy@gmail.com>
2015-03-18 13:36:12 -0700

    Create web1.go

---
+++ web1.go
@@ -0,0 +1,13 @@
+package static
+
+import (
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", static)
+}
+
+func static(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "public/"+r.URL.Path)
+}
